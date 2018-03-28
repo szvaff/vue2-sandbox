ba6e83bd65d0f77d2f6eafa83d27e5cc06f9826e
Péter Nagy <peter.nagy@itware.hu>
2018-03-28 11:25:07 +0200

    added base and baseUrl to index.html

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -68,7 +68,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency',
       serviceWorkerLoader: `<script>${loadMinified(path.join(__dirname,
-        './service-worker-prod.js'))}</script>`
+        './service-worker-prod.js'))}</script>`,
+      baseUrl: '/vue2-sandbox'
     }),
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
